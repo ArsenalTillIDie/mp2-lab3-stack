c65b55a1ca9b2f1af6ed15ee6755af727a6daa36
Орлов Максим Денисович <orlov.maxim2001@gmail.com>
2020-11-07 20:27:21 +0300

    Solved problems 1 and 3

--- samples/sample_stack.cpp
+++ samples/sample_stack.cpp
@@ -1,33 +1,94 @@
-// ННГУ, ВМК, Курс "Методы программирования-2", С++, ООП
-//
-// sample_matrix.cpp - Copyright (c) Гергель В.П. 07.05.2001
-//   Переработано для Microsoft Visual Studio 2008 Сысоевым А.В. (20.04.2015)
-//
-// Тестирование верхнетреугольной матрицы
 
 #include <iostream>
-#include "utmatrix.h"
-//---------------------------------------------------------------------------
+#include "utstack.h"
 
-int main()
-{
-  TMatrix<int> a(5), b(5), c(5);
-  int i, j;
+const int problemNumber = 1;
 
-  setlocale(LC_ALL, "Russian");
-  cout << "Тестирование программ поддержки представления треугольных матриц"
-    << endl;
-  for (i = 0; i < 5; i++)
-    for (j = i; j < 5; j++ )
-    {
-      a[i][j] =  i * 10 + j;
-      b[i][j] = (i * 10 + j) * 100;
-    }
-  c = a + b;
-  cout << "Matrix a = " << endl << a << endl;
-  cout << "Matrix b = " << endl << b << endl;
-  cout << "Matrix c = a + b" << endl << c << endl;
-  
-  return 0;
+// Задача 1 
+const int nBracketTypes = 3;
+const char brackets[nBracketTypes][2] = { {'(', ')'},{'{', '}'},{'[', ']'} };
+
+char closingBracket(const char openingBracket) {
+    for (int i = 0; i < nBracketTypes; i++)
+        if (brackets[i][0] == openingBracket)
+            return brackets[i][1];
+    return '\0';
 }
-//---------------------------------------------------------------------------
+
+char openingBracket(const char closingBracket) {
+    for (int i = 0; i < nBracketTypes; i++)
+        if (brackets[i][1] == closingBracket)
+            return brackets[i][0];
+    return '\0';
+}
+
+void bracketChecker(int argc, char* argv[])
+{
+    if (argc > 1) {
+        TStack<char> bracketStack(100);
+        bool mistakeFlag = false;
+        for (int i = 0; i < strlen(argv[1]); i++) {
+            if (closingBracket(argv[1][i]) != '\0')
+                bracketStack.push(argv[1][i]);
+            if (openingBracket(argv[1][i]) != '\0') {
+                if (bracketStack.empty()) mistakeFlag = true;
+                else if (argv[1][i] != closingBracket(bracketStack.top())) mistakeFlag = true;
+                else bracketStack.pop();
+            }
+        }
+        if (!bracketStack.empty()) mistakeFlag = true;
+        std::cout << "The brackets in expression " << argv[1] << " are placed ";
+        if (mistakeFlag) std::cout << "incorrectly\n";
+        else std::cout << "correctly\n";
+    }
+    else std::cout << "No expression found\n";
+}
+
+// Задача 3
+
+template<class T> class StackWithMinimum : public TStack<T> {
+private:
+    TStack<T> minimums;
+public:
+    StackWithMinimum() : TStack<T>() {
+        minimums.TStack::TStack();
+    }
+    StackWithMinimum(int maxs) : TStack<T>(maxs) {
+        minimums.TStack::TStack(maxs);
+    }
+    StackWithMinimum(const TStack& s) : TStack<T>(s) {
+        minimums.TStack::TStack(s);
+    }
+    void push(T elem) {
+        if (empty() || elem < minimums.top()) minimums.push(elem);
+        else minimums.push(minimums.top());
+        TStack<T>::push(elem);
+    }
+    void pop() {
+        minimums.pop();
+        TStack<T>::pop();
+    }
+    int minimum() {
+        return minimums.top();
+    }
+};
+
+int main(int argc, char* argv[]) {
+    switch (problemNumber) {
+    case 1: {
+        bracketChecker(argc, argv);
+        break;
+    }
+    case 3: {
+        StackWithMinimum<int> swm(5);
+        swm.push(3); std::cout << swm.minimum() << std::endl;
+        swm.push(5); std::cout << swm.minimum() << std::endl;
+        swm.push(1); std::cout << swm.minimum() << std::endl;
+        swm.push(2); std::cout << swm.minimum() << std::endl;
+        swm.pop(); std::cout << swm.minimum() << std::endl;
+        swm.pop(); std::cout << swm.minimum() << std::endl;
+        swm.pop(); std::cout << swm.minimum() << std::endl;
+        break;
+    }
+    }
+}
